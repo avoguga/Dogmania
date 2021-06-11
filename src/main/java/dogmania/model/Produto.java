46d139ceb3f5bb3707b03df5fb2a2b20dbd28fd3
Gustavo Henrique <avoguga@gmail.com>
2021-06-11 04:34:13 -0300

    mapeando classes

---
+++ src/main/java/dogmania/model/Produto.java
@@ -0,0 +1,32 @@
+package dogmania.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name = "animal_id")
+	private Long animalId;
+	
+	private String nome;
+	
+	public void setAnimalId(Long animalId) {
+		this.animalId = animalId;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
