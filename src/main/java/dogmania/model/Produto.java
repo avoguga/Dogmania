1b3319611254205240611452d31753d30192561c
Gustavo Henrique <avoguga@gmail.com>
2021-06-15 21:48:17 -0300

    getters and setters produto

--- src/main/java/dogmania/model/Produto.java
+++ src/main/java/dogmania/model/Produto.java
@@ -1,5 +1,7 @@
 package dogmania.model;
 
+import java.beans.BeanProperty;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,8 +16,19 @@ public class Produto {
 	
 	@Column(name = "produto_id")
 	private Long produtoId;
-	
 	private String nome;
+	private Integer quantidade;
+	@Column(name="valor_compra")
+	private Float valorCompra;
+	@Column(name="valor_venda")
+	private Float valorVenda;
+	private Date fabricacao;
+	private Date validade;
+	private String categoria;
+	private Marca marca;
+	@Column(name="forncededor_id")
+	private Integer fornecedorId;
+
 	
 	public void setProdutoId(Long produtoId) {
 		this.produtoId = produtoId;
@@ -27,5 +40,69 @@ public class Produto {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getValorCompra() {
+		return valorCompra;
+	}
+
+	public void setValorCompra(Float valorCompra) {
+		this.valorCompra = valorCompra;
+	}
+
+	public String getValorVenda() {
+		return valorCompra;
+	}
+
+	public void setValorVenda(Float valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public String getFabricacao() {
+		return fabricacao;
+	}
+
+	public void setFabricacao(Date fabricacao) {
+		this.fabricacao = fabricacao;
+	}
+
+	public String getValidade() {
+		return fabricacao;
+	}
+
+	public void setValidade(Date validade) {
+		this.validade = validade;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getFornecedor() {
+		return fornecedorId;
+	}
+
+	public void setFornecedor(Integer fornecedorId) {
+		this.fornecedorId = fornecedorId;
 	}
 }
